79d3d7246ee4fbbc366cb502c938b7b4544635da
DanielGogu <danielgogubz@gmail.com>
2024-03-13 19:51:46 +0100

    i worked on planet 2

--- src/test/java/SolarSystem.java
+++ src/test/java/SolarSystem.java
@@ -8,6 +8,9 @@ public class SolarSystem {
         xros.Color="pink";
         xros.Size="21314654651miles";
         xros.temp="31656546k";
-        
+        Planet2 pln2=new Planet2();
+        pln2.size="6546464km";
+        pln2.temp="-1654654c";
+        pln2.color="red";
     }
 }
